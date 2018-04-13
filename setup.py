134c3bf436d1274978f22d4d543d45575b7d8e4a
HongXiang <hx.hongxiang@gmail.com>
2018-04-13 17:56:56 +0800

    fix namespace packages

--- setup.py
+++ setup.py
@@ -1,13 +1,14 @@
 from setuptools import setup, find_packages
 setup(
     name='dxl-medimage',
-    version='0.0.1',
+    version='0.0.3',
     description='Medical Image Processing Library.',
     url='https://github.com/Hong-Xiang/dxmedimage',
     author='Hong Xiang',
     author_email='hx.hongxiang@gmail.com',
     license='MIT',
-    packages=['dxl.medimage'],
+    namespace_packages=['dxl'],
+    packages=find_packages('src/python'), 
     package_dir={'': 'src/python'},
     install_requires=[],
     scripts=[],
